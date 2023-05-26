f63e72c95bb2e1fad4d0ee79ae07e77dddd6e988
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-26 21:16:01 +0800

    added function for input and output

--- Multiline.py
+++ Multiline.py
@@ -3,4 +3,7 @@ import sys
 print(sys.path)
 
 with open("C:/Users/Tou/Desktop/School/OOP/Python Stuff/Multiline/mylife.txt") as line_output:
-    
+
+    # Create a function to add initial line
+    line_input =  input("PLease write the line you desire: ")
+    line_output.write(line_input + "\n")
