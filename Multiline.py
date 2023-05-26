bcaffea181d20b7e4193ee223fd184e5b3a6540d
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-26 21:29:05 +0800

    added input into the code

--- Multiline.py
+++ Multiline.py
@@ -5,5 +5,11 @@ print(sys.path)
 with open("C:/Users/Tou/Desktop/School/OOP/Python Stuff/Multiline/mylife.txt") as line_output:
 
     # Create a function to add initial line
-    line_input =  input("PLease write the line you desire: ")
+    line_input = input("PLease write the line you desire: ")
     line_output.write(line_input + "\n")
+
+    print("Would you like to continue? y/n?")
+
+    # Setup decision input for the loop function later
+    decision = input(str("y for yes, n for no: "))
+    decision = decision.lower()
